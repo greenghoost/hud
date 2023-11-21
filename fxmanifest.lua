f3f0d8f1bbab19c8718148fcab57b3482a5ac478
greenghoost <113727619+greenghoost@users.noreply.github.com>
2023-11-22 02:37:32 +0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,35 @@
+fx_version 'cerulean'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+game 'gta5'
+name 'qbx_hud'
+author 'Qbus & Qbox Contributors'
+version '1.0.0'
+repository 'https://github.com/Qbox-project/qbx_hud'
+description 'HUD for Qbox'
+
+shared_scripts {
+    '@ggcore/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua',
+    'config.lua',
+    '@ox_lib/init.lua',
+    '@ggcore/import.lua',
+}
+
+modules {
+    'ggcore:playerdata',
+}
+
+server_script 'server.lua'
+client_script 'client.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/*',
+    'html/index.html',
+    'html/styles.css',
+    'html/responsive.css',
+    'html/app.js',
+}
---
+++ server.lua
@@ -0,0 +1,89 @@
+local resetStress = false
+
+-- Handlers
+
+AddEventHandler('ox_inventory:openedInventory', function(source)
+    TriggerClientEvent('qbx_hud:client:hideHud', source)
+end)
+
+AddEventHandler('ox_inventory:closedInventory', function(source)
+    TriggerClientEvent('qbx_hud:client:showHud', source)
+end)
+
+-- Callbacks
+
+lib.callback.register('hud:server:getMenu', function()
+    return Config.Menu
+end)
+
+-- Network Events
+
+RegisterNetEvent('hud:server:GainStress', function(amount)
+    local src = source
+    local Player = exports.ggcore:GetPlayer(src)
+    local newStress
+    if not Player or (Config.DisablePoliceStress and Player.PlayerData.job.type == 'leo') then return end
+    if not resetStress then
+        if not Player.PlayerData.metadata.stress then
+            Player.PlayerData.metadata.stress = 0
+        end
+        newStress = Player.PlayerData.metadata.stress + amount
+        if newStress <= 0 then newStress = 0 end
+    else
+        newStress = 0
+    end
+    if newStress > 100 then
+        newStress = 100
+    end
+    Player.Functions.SetMetaData('stress', newStress)
+    TriggerClientEvent('hud:client:UpdateStress', src, newStress)
+    TriggerClientEvent('QBCore:Notify', src, Lang:t("notify.stress_gain"), 'inform', 2500, nil, nil, {'#141517', '#ffffff'}, 'brain', '#C53030')
+end)
+
+RegisterNetEvent('hud:server:RelieveStress', function(amount)
+    local src = source
+    local Player = exports.ggcore:GetPlayer(src)
+    local newStress
+    if not Player then return end
+    if not resetStress then
+        if not Player.PlayerData.metadata.stress then
+            Player.PlayerData.metadata.stress = 0
+        end
+        newStress = Player.PlayerData.metadata.stress - amount
+        if newStress <= 0 then newStress = 0 end
+    else
+        newStress = 0
+    end
+    if newStress > 100 then
+        newStress = 100
+    end
+    Player.Functions.SetMetaData('stress', newStress)
+    TriggerClientEvent('hud:client:UpdateStress', src, newStress)
+    TriggerClientEvent('QBCore:Notify', src, Lang:t("notify.stress_removed"), 'inform', 2500, nil, nil, {'#141517', '#ffffff'}, 'brain', '#0F52BA')
+end)
+
+-- Commands
+
+lib.addCommand(Lang:t('commands.cash'), {
+    help = Lang:t('commands.help.cash'),
+    restricted = 'group.admin'
+}, function(source)
+    local player = exports.ggcore:GetPlayer(source)
+    local cashAmount = player.PlayerData.money.cash
+    TriggerClientEvent('hud:client:ShowAccounts', source, 'cash', cashAmount)
+end)
+
+lib.addCommand(Lang:t('commands.bank'), {
+    help = Lang:t('commands.help.bank'),
+}, function(source)
+    local player = exports.ggcore:GetPlayer(source)
+    local bankAmount = player.PlayerData.money.bank
+    TriggerClientEvent('hud:client:ShowAccounts', source, 'bank', bankAmount)
+end)
+
+lib.addCommand('dev', {
+    help = Lang:t('commands.help.dev'),
+    restricted = 'group.admin'
+}, function(source)
+    TriggerClientEvent("qb-admin:client:ToggleDevmode", source)
+end)
